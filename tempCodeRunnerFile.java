52ff54fd758d0e00538295e35691292287e902df
Aditi15042006 <aditisahu152006@gmail.com>
2025-12-14 13:03:40 +0530

    Update tempCodeRunnerFile.java

--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,10 +1,1 @@
-class pattern1 {
-    public static void main(string[] args) {
 
-        for(int i=0; i<5; i++){
-            for(int j=0; j<5; j++){
-                System.out.print("*");
-            }
-        }
-    }
-}
